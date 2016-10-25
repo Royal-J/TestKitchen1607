935522c5d0c8017b47cc4f85402317801a17bbf7
hao <123@126.com>
2016-10-25 12:04:19 +0800

    首页食材推荐的banner和猜你喜欢

---
+++ TestKitchen1607/TestKitchen1607/classes/ingredient(食材)/main(主要模块)/view/IngreRecommendView.swift
@@ -0,0 +1,72 @@
+//
+//  IngreRecommendView.swift
+//  TestKitchen1607
+//
+//  Created by HJY on 2016/10/25.
+//  Copyright © 2016年 HJY. All rights reserved.
+//
+
+import UIKit
+
+class IngreRecommendView: UIView {
+
+    //数据
+    var model: IngreRecommend? {
+        didSet {
+            //set方法调用之后会调用这里的方法
+            tbView?.reloadData()
+        }
+    }
+    
+    //表格
+    private var tbView: UITableView?
+    
+    //重新实现初始化方法
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //创建表格视图
+        tbView = UITableView(frame: CGRectZero, style: .Plain)
+        tbView?.delegate = self
+        tbView?.dataSource = self
+        //tbView?.backgroundColor = UIColor.blueColor()
+        addSubview(tbView!)
+        
+        //约束
+        tbView?.snp_makeConstraints(closure: { (make) in
+            make.edges.equalToSuperview()
+        })
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+
+//MARK:UITableView代理
+extension IngreRecommendView: UITableViewDelegate,  UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //banner广告部分显示一个分组
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //banner广告的section显示一行
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        //banner广告高度为140
+        return 140
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+                
+        let cell = IngreBannerCell.createBannerCellFor(tableView, atIndexPath: indexPath, bannerArray: model?.data!.bannerArray)
+        return cell
+    }
+}
---
+++ TestKitchen1607/TestKitchen1607/classes/ingredient(食材)/recommend(推荐)/view/IngreLikeCell.swift
@@ -0,0 +1,26 @@
+//
+//  IngreLikeCell.swift
+//  TestKitchen1607
+//
+//  Created by HJY on 2016/10/25.
+//  Copyright © 2016年 HJY. All rights reserved.
+//
+
+import UIKit
+
+class IngreLikeCell: UITableViewCell {
+
+    @IBAction func clickBtn(sender: UIButton) {
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
